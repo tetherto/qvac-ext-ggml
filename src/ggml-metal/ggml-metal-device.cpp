c5a9b753ef85f6451f55769d361973ed9404315b
Marcus Edel <marcus.edel@collabora.com>
2026-04-08 07:55:51 +0200

    Fix type mismatch errors in Metal backend.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- src/ggml-metal/ggml-metal-device.cpp
+++ src/ggml-metal/ggml-metal-device.cpp
@@ -2008,11 +2008,9 @@ ggml_metal_pipeline_with_params ggml_metal_library_get_pipeline_out_prod(ggml_me
     snprintf(name, 256, "%s", base);
 
     ggml_metal_pipeline_with_params res = ggml_metal_library_get_pipeline(lib, name);
-    if (res) {
-        return res;
+    if (!res.pipeline) {
+        res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
     }
-
-    res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
 
     return res;
 }
@@ -2030,11 +2028,9 @@ ggml_metal_pipeline_with_params ggml_metal_library_get_pipeline_silu_back(ggml_m
     snprintf(name, 256, "%s", base);
 
     ggml_metal_pipeline_with_params res = ggml_metal_library_get_pipeline(lib, name);
-    if (res) {
-        return res;
+    if (!res.pipeline) {
+        res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
     }
-
-    res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
 
     return res;
 }
@@ -2051,13 +2047,11 @@ ggml_metal_pipeline_with_params ggml_metal_library_get_pipeline_soft_max_back(gg
     snprintf(name, 256, "%s", base);
 
     ggml_metal_pipeline_with_params res = ggml_metal_library_get_pipeline(lib, name);
-    if (res) {
-        return res;
+    if (!res.pipeline) {
+        res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
     }
 
-    res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
-
-    ggml_metal_pipeline_set_smem(res, 32*sizeof(float));
+    res.smem = 32*sizeof(float);
 
     return res;
 }
@@ -2073,13 +2067,11 @@ ggml_metal_pipeline_with_params ggml_metal_library_get_pipeline_rms_norm_back(gg
     snprintf(name, 256, "%s", base);
 
     ggml_metal_pipeline_with_params res = ggml_metal_library_get_pipeline(lib, name);
-    if (res) {
-        return res;
+    if (!res.pipeline) {
+        res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
     }
 
-    res = ggml_metal_library_compile_pipeline(lib, base, name, nullptr);
-
-    ggml_metal_pipeline_set_smem(res, 2*32*sizeof(float));
+    res.smem = 2*32*sizeof(float);
 
     return res;
 }
